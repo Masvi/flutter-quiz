26b1e0d61a76cb8d663b85b67749ab46ca6ea53c
Masvi <jeanmasvi@gmail.com>
2021-04-21 21:18:07 -0300

    feat: adds buttons

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/levelbutton/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +17,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWiddget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(label: "Fácil"),
+            LevelButtonWidget(label: "Médio"),
+            LevelButtonWidget(label: "Difícil"),
+            LevelButtonWidget(label: "Perito"),
+          ]
+        ),
+      ),
     );
   }
 }
